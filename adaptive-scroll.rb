47f9aa6a98b9b9b476c59dbaa70e093f8ddfe803
Anka <runner@Mac-1760221227070.local>
2025-10-11 22:32:14 +0000

    Update adaptive-scroll to v0.0.11

--- adaptive-scroll.rb
+++ adaptive-scroll.rb
@@ -1,9 +1,9 @@
 class AdaptiveScroll < Formula
   desc "Adaptive scroll direction for macOS based on external mouse connection"
   homepage "https://github.com/narasaka/adaptive-scroll"
-  url "https://github.com/narasaka/adaptive-scroll/releases/download/v0.0.10/adaptive-scroll"
+  url "https://github.com/narasaka/adaptive-scroll/releases/download/v0.0.11/adaptive-scroll"
   sha256 "6d7c9badb4c92edecf2b654d380d828654b9cc800e17151085cf3c36af031284"
-  version "v0.0.10"
+  version "v0.0.11"
 
   def install
     bin.install "adaptive-scroll"
