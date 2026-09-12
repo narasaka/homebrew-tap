5d9b95b662009f50b4edb3d0e7e812776e6292c1
Nathanael Tehilla Gunawan <me@narasaka.dev>
2026-09-12 04:00:05 -0400

    kamui: update to v0.0.2

--- kamui.rb
+++ kamui.rb
@@ -1,8 +1,8 @@
 class Kamui < Formula
   desc "Use a remote SSH host's loopback services in a development browser"
   homepage "https://github.com/narasaka/kamui"
-  url "https://github.com/narasaka/kamui/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "9dde3e93b1c4c31ce55e2c3c3de223af28585532f28b542f32b437ddd30fbf71"
+  url "https://github.com/narasaka/kamui/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "b938c839eea01a73558d676b21c378750fa5ab70ccd8805e8747be9f82e326af"
   license "MIT"
 
   depends_on "go" => :build
@@ -11,8 +11,8 @@ class Kamui < Formula
   def install
     ldflags = "-buildid= " \
               "-X github.com/narasaka/kamui/internal/version.Version=#{version} " \
-              "-X github.com/narasaka/kamui/internal/version.Commit=8eebd46f008f92ba84b0fff48a4cde104259ac43 " \
-              "-X github.com/narasaka/kamui/internal/version.BuildDate=2026-09-12T00:42:12-04:00"
+              "-X github.com/narasaka/kamui/internal/version.Commit=7ba8df5c7f0adf0d654b2c2eeffcb0d75545674f " \
+              "-X github.com/narasaka/kamui/internal/version.BuildDate=2026-09-11T23:59:35-07:00"
     system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/kamui"
   end
 
